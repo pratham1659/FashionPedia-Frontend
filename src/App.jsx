da8c40a71c75afaa1ece5aa93299ac5e303a96d7
prathamkumar <prathamkumar1985@gmail.com>
2023-12-22 20:09:18 +0530

    Minor Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
+
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+import Home from "./pages/home/Home";
+import PageNotFound from "./pages/error/pageNotFound";
+
 function App() {
   return (
     <>
-      <div>App JS</div>
+      <BrowserRouter>
+        <LoadingBar
+          color="#08b4e3"
+          height={3}
+          progress={"NAN"}
+          onLoaderFinished={"NAN"}
+        />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/error" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
