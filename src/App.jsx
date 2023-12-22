44d97a39061bdc10fbef8ccd8492071c698dd46d
prathamkumar <prathamkumar1985@gmail.com>
2023-12-22 19:39:19 +0530

    Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
-
 function App() {
   return (
     <>
-      <Navbar />
+      <div>App JS</div>
     </>
   );
 }
